20be2aa2be783b024b1dbb255b9e23881a67ce16
Farazode <149819099+Farazode@users.noreply.github.com>
2024-06-06 16:02:47 +0200

    simple

--- app.js
+++ app.js
@@ -4,8 +4,12 @@ document.addEventListener('DOMContentLoaded', () => {
     getInviteLinkButton.addEventListener('click', () => {
       console.log('Get Invite Link button clicked');
       try {
-        Telegram.WebApp.sendData('get_invite_link');
-        console.log('Data sent to bot: get_invite_link');
+        if (window.Telegram && window.Telegram.WebApp) {
+          Telegram.WebApp.sendData('get_invite_link');
+          console.log('Data sent to bot: get_invite_link');
+        } else {
+          console.error('Telegram WebApp object not available');
+        }
       } catch (error) {
         console.error('Error sending data:', error);
       }
