2a32ffc56d28fb59160f02fdda98dec11f1e8493
Farazode <149819099+Farazode@users.noreply.github.com>
2024-06-06 16:00:28 +0200

    simple

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
   const getInviteLinkButton = document.getElementById('getInviteLinkButton');
-  getInviteLinkButton.addEventListener('click', () => {
-    console.log('Get Invite Link button clicked');
-    try {
-      Telegram.WebApp.sendData('get_invite_link');
-      console.log('Data sent to bot: get_invite_link');
-    } catch (error) {
-      console.error('Error sending data:', error);
-    }
-  });
+  if (getInviteLinkButton) {
+    getInviteLinkButton.addEventListener('click', () => {
+      console.log('Get Invite Link button clicked');
+      try {
+        Telegram.WebApp.sendData('get_invite_link');
+        console.log('Data sent to bot: get_invite_link');
+      } catch (error) {
+        console.error('Error sending data:', error);
+      }
+    });
+  } else {
+    console.error('getInviteLinkButton not found');
+  }
 });
